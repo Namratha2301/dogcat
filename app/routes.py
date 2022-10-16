f2dcc0d74f02f6c8d935515e9b8b801151fa85d6
NarayanAdithya <narayanadithya1234@gmail.com>
2022-10-16 10:58:23 +0530

    Completed Route for Model

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from flask import request, render_template
 from app import app
+from app.modelutils import instant_predict
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        # Code to Predict From Aud
-        pass
+        aud = request.files['audio_data']
+        val = instant_predict(aud)[0]
+        val = 'Cat' if val == 0 else 'Dog'
+        return val
     return render_template('home.html')
